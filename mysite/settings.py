e54e1f4ee19398be2f011313bd767400634af95d
Hesam-Coding <mohamadhesamgot@gmail.com>
2026-06-13 18:04:48 +0330

    added static directories and urls in settings
    static root
    static url
    media url
    media root

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'template'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- website/views.py
+++ website/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse    #پاسخ دادن به کاربر
 
-def index_view(request):
-    return render(request ,'website/index.html')
-
+def about_view(request):
+    return render(request , 'website/index.html')
 
 def about_view(request):
     return render(request , 'website/about.html' )
 
 def contact_view(request):
     return render(request, 'website/contact.html')  
+
+
